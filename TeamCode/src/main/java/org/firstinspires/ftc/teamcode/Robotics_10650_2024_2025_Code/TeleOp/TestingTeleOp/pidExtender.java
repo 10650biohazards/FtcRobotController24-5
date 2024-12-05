d01fbbc27f462a94bb1703f23185d9939b1929d8
10650biohazards <hazmat10650@gmail.com>
2024-12-05 17:25:45 -0500

    Updated liftPitch to work better
    Changed velocity on going back up to be faster.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robotics_10650_2024_2025_Code/TeleOp/TestingTeleOp/pidExtender.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robotics_10650_2024_2025_Code/TeleOp/TestingTeleOp/pidExtender.java
@@ -5,8 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Robotics_10650_2024_2025_Code.InitializeFolder.RobotInitialize;
 
-@TeleOp(name = "pid")
-public class pidTele extends LinearOpMode {
+@TeleOp(name = "pidExtender")
+public class pidExtender extends LinearOpMode {
 
     // Run the initialize function
     RobotInitialize robot;
@@ -51,9 +51,9 @@ public class pidTele extends LinearOpMode {
             robot.liftExtender.setVelocity(0);
         }else if(Math.abs(robot.liftExtender.getCurrentPosition()-liftExtenderPosition)>25) {
             if (robot.liftExtender.getCurrentPosition() < liftExtenderPosition) {
-                robot.liftExtender.setVelocity(2000); //was 1500
+                robot.liftExtender.setVelocity(5000);
             } else if (robot.liftExtender.getCurrentPosition() >= liftExtenderPosition) {
-                robot.liftExtender.setVelocity(-2000); //was -1500
+                robot.liftExtender.setVelocity(-5000);
             }
             //If no input, make sure the liftExtender motor does not move
         }else {
